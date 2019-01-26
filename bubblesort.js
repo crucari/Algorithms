70242b7f113f1cf32204a5cc5174dffda9f71c24
Carina Cruz <lecruz@bu.edu>
2019-01-26 19:49:53 +0000

    bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,9 @@
+function bubbleSort(array) {
+  // change code below this line
+
+  // change code above this line
+  return array;
+}
+
+// test array:
+// [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
